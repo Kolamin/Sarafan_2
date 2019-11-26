738a2e2fe63940a897824ecf6afd4d5af4fd2001
Kolamin <gofranich@gmail.com>
2019-11-26 15:44:16 +0600

    Настраиваем Spring Security и oAuth2 в Vue.js. Spring Boot REST

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,5 @@
 function getIndex(list, id) {
-    for (var i = 0; i < list.length; i++ ) {
+    for (var i = 0; i < list.length; i++) {
         if (list[i].id === id) {
             return i;
         }
@@ -20,19 +20,19 @@ Vue.component('message-form', {
         }
     },
     watch: {
-        messageAttr: function(newVal, oldVal) {
+        messageAttr: function (newVal, oldVal) {
             this.text = newVal.text;
             this.id = newVal.id;
         }
     },
     template:
         '<div>' +
-            '<input type="text" placeholder="Write something" v-model="text" />' +
-            '<input type="button" value="Save" @click="save" />' +
+        '<input type="text" placeholder="Write something" v-model="text" />' +
+        '<input type="button" value="Save" @click="save" />' +
         '</div>',
     methods: {
-        save: function() {
-            var message = { text: this.text };
+        save: function () {
+            var message = {text: this.text};
 
             if (this.id) {
                 messageApi.update({id: this.id}, message).then(result =>
@@ -60,15 +60,15 @@ Vue.component('message-row', {
     template: '<div>' +
         '<i>({{ message.id }})</i> {{ message.text }}' +
         '<span style="position: absolute; right: 0">' +
-            '<input type="button" value="Edit" @click="edit" />' +
-            '<input type="button" value="X" @click="del" />' +
+        '<input type="button" value="Edit" @click="edit" />' +
+        '<input type="button" value="X" @click="del" />' +
         '</span>' +
         '</div>',
     methods: {
-        edit: function() {
+        edit: function () {
             this.editMethod(this.message);
         },
-        del: function() {
+        del: function () {
             messageApi.remove({id: this.message.id}).then(result => {
                 if (result.ok) {
                     this.messages.splice(this.messages.indexOf(this.message), 1)
@@ -79,36 +79,46 @@ Vue.component('message-row', {
 });
 
 Vue.component('messages-list', {
-  props: ['messages'],
-  data: function() {
-    return {
-        message: null
-    }
-  },
-  template:
-    '<div style="position: relative; width: 300px;">' +
+    props: ['messages'],
+    data: function () {
+        return {
+            message: null
+        }
+    },
+    template:
+        '<div style="position: relative; width: 300px;">' +
         '<message-form :messages="messages" :messageAttr="message" />' +
         '<message-row v-for="message in messages" :key="message.id" :message="message" ' +
-            ':editMethod="editMethod" :messages="messages" />' +
-    '</div>',
-  created: function() {
-    messageApi.get().then(result =>
-        result.json().then(data =>
-            data.forEach(message => this.messages.push(message))
-        )
-    )
-  },
-  methods: {
-    editMethod: function(message) {
-        this.message = message;
+        ':editMethod="editMethod" :messages="messages" />' +
+        '</div>',
+
+    methods: {
+        editMethod: function (message) {
+            this.message = message;
+        }
     }
-  }
 });
 
 var app = new Vue({
-  el: '#app',
-  template: '<messages-list :messages="messages" />',
-  data: {
-    messages: []
-  }
+    el: '#app',
+    template:
+        '<div>' +
+            '<div v-if="!profile">Необходимо авторизоваться через <a href="/login">Google</a></div>'+
+            '<div v-else >' +
+                '<div>{{profile.name}}&nbsp;<a href="/logout">Выйти</a></div>'+
+                '</div><messages-list :messages="messages" />' +
+            '</div>'+
+        '</div>',
+    data: {
+        messages: frontendData.messages,
+        profile: frontendData.profile
+    },
+
+    created: function () {
+        // messageApi.get().then(result =>
+        //     result.json().then(data =>
+        //         data.forEach(message => this.messages.push(message))
+        //     )
+        // )
+    },
 });
